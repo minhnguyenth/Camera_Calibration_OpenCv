862afcece84037afc7dba13c877c0bdf859a4de3
minhnguyenxd <nguyenanhminhxd@gmail.com>
2018-02-23 13:22:58 -0800

    Initial commit

---
+++ Calibrator.h
@@ -0,0 +1,27 @@
+
+/* 
+ * File:   Calibrator.h
+ * Author: mnguyen
+ *
+ * Created on February 23, 2018, 1:12 PM
+ */
+
+#ifndef CALIBRATOR_H
+#define CALIBRATOR_H
+
+#include <vector>
+#include <iostream>
+
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/highgui.hpp>
+
+class Calibrator {
+
+
+};
+
+
+#endif /* CALIBRATOR_H */
+
---
+++ main.cpp
@@ -0,0 +1,17 @@
+
+/* 
+ * File:   main.cpp
+ * Author: minh nguyen
+ *
+ * Created on February 23, 2018, 12:32 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
